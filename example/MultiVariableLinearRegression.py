45109e8f9159753565c267201627da6ab616f611
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-05-12 00:38:27 +0900

    loss rate 그래프 추가

--- example/MultiVariableLinearRegression.py
+++ example/MultiVariableLinearRegression.py
@@ -2,6 +2,15 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import inspect
+import os
+
+plt.style.use("seaborn-whitegrid")
+
+file_name = inspect.getfile(inspect.currentframe()).split('/')[-1]
+file_ = file_name.split('.')[0]
+
 x_train = torch.FloatTensor([[73,80,75],
                              [93,88,93],
                              [89,91,90],
@@ -38,11 +47,19 @@ model = MultiLinearReg()
 optimizer = optim.SGD(model.parameters(),lr=0.00001)
 
 epoch = 30
+plot_x = range(epoch)
+plot_cost = []
 for i in range(1,epoch+1):
     hx = model(x_train)
     cost = F.mse_loss(hx,y_train)
-    
+    plot_cost.append(cost.detach())
+
     optimizer.zero_grad()
     cost.backward()
     optimizer.step()
     print(f"epoch : {i}/{epoch} \t\t cost : {cost} \t\t hx:{hx.squeeze().detach()}")
+
+plt.plot(plot_x,plot_cost,'--r')
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.savefig(f"{os.getcwd()}\\example\\source\\{file_}_loss.png")
